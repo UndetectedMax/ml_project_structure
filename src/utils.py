ee560f407c38c8c9496f1836585d69d8f20e1031
UndetectedMax <maxm13052004@gmail.com>
2025-08-06 19:36:20 +0300

    Added all for user interactions

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
 
 
@@ -39,6 +40,12 @@ def evaluate_model(x_train,y_train,y_test, x_test, models):
     except Exception as e:
         raise CustomException(e, sys) 
 
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
+    except Exception as e:
+        raise CustomException(e, sys)
 
 
